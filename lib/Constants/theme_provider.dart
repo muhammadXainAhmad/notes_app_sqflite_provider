54baa2bd97ba3c3a348fef43f38d50a196ad5634
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-18 17:34:10 +0500

    Integrated sharedPreferences to persist dark mode state

--- lib/Constants/theme_provider.dart
+++ lib/Constants/theme_provider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool _isDarkMode = false;
-  
-  bool getThemeValue()=>_isDarkMode;
-  
-  void updateTheme({required bool value}) {
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+  bool getThemeValue() => _isDarkMode;
+
+  void updateTheme({required bool value}) async {
     _isDarkMode = value;
+    notifyListeners();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("isDarkMode", _isDarkMode);
+  }
+
+  void _loadTheme() async {
+    var pref = await SharedPreferences.getInstance();
+    _isDarkMode = pref.getBool("isDarkMode") ?? false;
     notifyListeners();
   }
 }
